150f80f8bb6cb99e22119f26cc7dfe19e5da6d15
Hubert Siwkin <hsiwkin@gmail.com>
2019-12-25 00:22:07 +0100

    Fixes for heroku
    Signed-off-by: Hubert Siwkin <hsiwkin@gmail.com>

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-//Install express server
 const express = require("express");
-
 const path = require("path");
 
 const app = express();
+const buildPath = path.join(__dirname, "dist", "ciastka-frontend");
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(buildPath));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
