8b47a87e48ebe06ebc8c5c3bc5bc455ceab10572
Hubert Siwkin <hsiwkin@gmail.com>
2019-12-25 00:49:23 +0100

    Add offers component, links in top-bar and styles everywhere
    Signed-off-by: Hubert Siwkin <hsiwkin@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { OfferListComponent } from "./offer-list/offer-list.component";
 
 const routes: Routes = [
   {
     path: "",
     component: LandingPageComponent
+  },
+  {
+    path: "offers",
+    component: OfferListComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { OfferListComponent } from './offer-list/offer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    OfferListComponent
   ],
   imports: [
     BrowserModule,
