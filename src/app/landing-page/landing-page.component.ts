2125ee8a35d037d1c44bae02f499ec4c1563588f
Hubert Siwkin <hsiwkin@gmail.com>
2019-12-25 14:10:33 +0100

    move offers to JSON, add string interpolation in components

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { offers } from "../offers";
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  selector: "app-landing-page",
+  templateUrl: "./landing-page.component.html",
+  styleUrls: ["./landing-page.component.scss"]
 })
 export class LandingPageComponent implements OnInit {
+  offers = offers;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/offer-list/offer-list.component.ts
+++ src/app/offer-list/offer-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { offers } from "../offers";
 
 @Component({
-  selector: 'app-offer-list',
-  templateUrl: './offer-list.component.html',
-  styleUrls: ['./offer-list.component.scss']
+  selector: "app-offer-list",
+  templateUrl: "./offer-list.component.html",
+  styleUrls: ["./offer-list.component.scss"]
 })
 export class OfferListComponent implements OnInit {
+  offers = offers;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
