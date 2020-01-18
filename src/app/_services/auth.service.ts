06cdae5b7e4db55054194282e0b9dcfbf534283e
Hubert Siwkin <hsiwkin@gmail.com>
2020-01-18 17:30:17 +0100

    Make login work

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import * as path from "path";
+import { IAuth } from "../_interfaces/auth.interface";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
+  API_ENDPOINT: string = "https://ciastkazonback.herokuapp.com/api/auth";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  logIn(authData: IAuth): Observable<any> {
+    const loginPath = path.join(this.API_ENDPOINT, "signin");
+    return this.http
+      .post(loginPath, {
+        password: authData.password,
+        usernameOrEmail: authData.email
+      })
+      .pipe(
+        tap(result => {
+          // TODO
+          console.log("handle hash", result);
+        })
+      );
+  }
+
+  signUp(authData: IAuth): Observable<any> {
+    // TODO
+    return null;
+  }
 }
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormControl, Validators } from "@angular/forms";
+import { AuthService } from "../_services/auth.service";
+import { IAuth } from "../_interfaces/auth.interface";
 
 @Component({
   selector: "app-auth",
@@ -9,17 +11,21 @@ import { FormBuilder, FormControl, Validators } from "@angular/forms";
 export class AuthComponent implements OnInit {
   authForm;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
     this.authForm = formBuilder.group({
-      email: new FormControl("", [Validators.required, Validators.email]),
+      email: new FormControl("", Validators.required),
       password: new FormControl("", Validators.required)
     });
   }
 
   ngOnInit() {}
 
-  onSubmit(authData) {
-    console.log("submit", authData);
-    // TODO: connect with server
+  onSubmit(authData: IAuth) {
+    if (this.authForm.valid) {
+      this.authService.logIn(authData).subscribe(result => console.log(result));
+    }
   }
 }
